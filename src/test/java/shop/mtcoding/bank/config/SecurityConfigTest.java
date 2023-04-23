828813a1fde603874b203fb8b7abc652a8334020
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-23 21:49:52 +0900

    feat: add common DTO

--- src/test/java/shop/mtcoding/bank/config/SecurityConfigTest.java
+++ src/test/java/shop/mtcoding/bank/config/SecurityConfigTest.java
@@ -2,6 +2,7 @@ package shop.mtcoding.bank.config;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,6 +18,8 @@ public class SecurityConfigTest {
       @Autowired // 가짜 환경에 등록된 MockMvc를 DI 함
       private MockMvc mvc;
 
+      // 서버는 일관성있게 에러가 리턴되어야 한다.
+      // 백엔드가 모르는 에러가 프론트한테 날라가지 않게, 내가 직접 다 제어하자.
       @Test
       public void authentication_test() throws Exception {
             // given
@@ -27,8 +30,9 @@ public class SecurityConfigTest {
             int httpStatusCode = resultActions.andReturn().getResponse().getStatus();
             System.out.println("테스트 : " + responseBody);
             System.out.println("테스트 : " + httpStatusCode);
-            // then
 
+            // then
+            Assertions.assertThat(httpStatusCode).isEqualTo(401);
       }
 
       @Test
@@ -36,8 +40,13 @@ public class SecurityConfigTest {
             // given
 
             // when
+            ResultActions resultActions = mvc.perform(get("/api/s/hello"));
+            String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+            int httpStatusCode = resultActions.andReturn().getResponse().getStatus();
+            System.out.println("테스트 : " + responseBody);
+            System.out.println("테스트 : " + httpStatusCode);
 
             // then
-
+            Assertions.assertThat(httpStatusCode).isEqualTo(401);
       }
 }

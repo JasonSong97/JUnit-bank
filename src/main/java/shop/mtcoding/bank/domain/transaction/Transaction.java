f310453ed026f092b1ca47a25ba08869d623a6e8
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-23 19:42:08 +0900

    design Transaction Entity

---
+++ src/main/java/shop/mtcoding/bank/domain/transaction/Transaction.java
@@ -0,0 +1,78 @@
+package shop.mtcoding.bank.domain.transaction;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.mtcoding.bank.domain.account.Account;
+
+@NoArgsConstructor
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "transaction_tb")
+@Entity
+public class Transaction {
+
+      @Id
+      @GeneratedValue(strategy = GenerationType.IDENTITY)
+      private Long id;
+
+      private Account withdrawAccount;
+      private Account depositAccount;
+
+      private Long amount;
+
+      // 히스토리 내역을 남기기 위해서 만든 필드
+      private Long withdrawAccountBalance; // 1111계좌 -> 1000 -> 500 -> 200
+      private Long depositAccountBalance;
+
+      @Enumerated(EnumType.STRING)
+      @Column(nullable = false)
+      private TransactionEnum gubun; // WITHDRAW, DEPOSIT, TRANSFER, ALL
+
+      // 계좌가 사라져도 로그는 남아야 한다.
+      private String sender;
+      private String receiver;
+      private String tel;
+
+      @CreatedDate
+      @Column(nullable = false)
+      private LocalDateTime createdAt;
+
+      @LastModifiedBy
+      @Column(nullable = false)
+      private LocalDateTime updatedAt;
+
+      @Builder
+      public Transaction(Long id, Account withdrawAccount, Account depositAccount, Long amount,
+                  Long withdrawAccountBalance, Long depositAccountBalance, TransactionEnum gubun, String sender,
+                  String receiver, String tel, LocalDateTime createdAt, LocalDateTime updatedAt) {
+            this.id = id;
+            this.withdrawAccount = withdrawAccount;
+            this.depositAccount = depositAccount;
+            this.amount = amount;
+            this.withdrawAccountBalance = withdrawAccountBalance;
+            this.depositAccountBalance = depositAccountBalance;
+            this.gubun = gubun;
+            this.sender = sender;
+            this.receiver = receiver;
+            this.tel = tel;
+            this.createdAt = createdAt;
+            this.updatedAt = updatedAt;
+      }
+}
--- src/main/java/shop/mtcoding/bank/domain/user/UserEnum.java
+++ src/main/java/shop/mtcoding/bank/domain/user/UserEnum.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum UserEnum {
-
       ADMIN("관리자"), CUSTOMER("고객");
 
       private String value;

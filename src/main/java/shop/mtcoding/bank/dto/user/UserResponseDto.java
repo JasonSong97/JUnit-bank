284727c72c4dc1dc4936083a9338efb90a7d99af
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-25 00:28:50 +0900

    회원가입 서비스 코드 리팩토링

---
+++ src/main/java/shop/mtcoding/bank/dto/user/UserResponseDto.java
@@ -0,0 +1,26 @@
+package shop.mtcoding.bank.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import shop.mtcoding.bank.domain.user.User;
+
+public class UserResponseDto {
+
+     @ToString
+     @Getter
+     @Setter
+     public static class JoinResponseDto {
+
+          private Long id;
+          private String username;
+          private String fullname;
+
+          public JoinResponseDto(User user) {
+               this.id = user.getId();
+               this.username = user.getUsername();
+               this.fullname = user.getFullname();
+          }
+
+     }
+}
--- src/test/java/shop/mtcoding/bank/service/UserServiceTest.java
+++ src/test/java/shop/mtcoding/bank/service/UserServiceTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -15,15 +14,15 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import shop.mtcoding.bank.config.dummy.DummyObject;
 import shop.mtcoding.bank.domain.user.User;
-import shop.mtcoding.bank.domain.user.UserEnum;
 import shop.mtcoding.bank.domain.user.UserRepository;
-import shop.mtcoding.bank.service.UserService.JoinRequestDto;
-import shop.mtcoding.bank.service.UserService.JoinResponseDto;
+import shop.mtcoding.bank.dto.user.UserRequestDto.JoinRequestDto;
+import shop.mtcoding.bank.dto.user.UserResponseDto.JoinResponseDto;
 
 // Spring 관련 Bean들이 하나도 없는 환경
 @ExtendWith(MockitoExtension.class) // 가짜 테스트
-public class UserServiceTest {
+public class UserServiceTest extends DummyObject {
 
      @InjectMocks // 가짜환경에다가 넣는다는 것
      private UserService userService;
@@ -49,16 +48,7 @@ public class UserServiceTest {
           // User()));
 
           // stub 2
-          User ssar = User.builder()
-                    .id(1L)
-                    .username("ssar")
-                    .password("1234")
-                    .email("ssa@nate.com")
-                    .fullname("쌀")
-                    .role(UserEnum.CUSTOMER)
-                    .createdAt(LocalDateTime.now())
-                    .updatedAt(LocalDateTime.now())
-                    .build();
+          User ssar = newMockUser(1L, "ssar", "쌀");
           when(userRepository.save(any())).thenReturn(ssar);
 
           // when

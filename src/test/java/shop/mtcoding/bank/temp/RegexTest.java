45d9c6523203f43abd997b7a12bb3049c299aa0c
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-25 01:43:01 +0900

    회원가입 컨트롤러 정규표현식 추가 2

--- src/test/java/shop/mtcoding/bank/temp/RegexTest.java
+++ src/test/java/shop/mtcoding/bank/temp/RegexTest.java
@@ -16,27 +16,37 @@ public class RegexTest {
 
      @Test
      public void 한글은안된다_test() throws Exception {
-
+          String value = "가";
+          boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]*$", value);
+          System.out.println("테스트 : " + result);
      }
 
      @Test
      public void 영어만된다_test() throws Exception {
-
+          String value = "asd";
+          boolean result = Pattern.matches("^[a-zA-Z]+$", value);
+          System.out.println("테스트 : " + result);
      }
 
      @Test
      public void 영어는안된다_test() throws Exception {
-
+          String value = "";
+          boolean result = Pattern.matches("^[^a-zA-Z]*$", value);
+          System.out.println("테스트 : " + result);
      }
 
      @Test
      public void 영어와숫자만된다_test() throws Exception {
-
+          String value = "";
+          boolean result = Pattern.matches("^[a-zA-Z0-9]+$", value);
+          System.out.println("테스트 : " + result);
      }
 
      @Test
      public void 영어만되고길이는최소2최대4이다_test() throws Exception {
-
+          String value = "asd1";
+          boolean result = Pattern.matches("^[a-zA-Z]{2,4}$", value);
+          System.out.println("테스트 : " + result);
      }
 
      // username, email, fullname test===================================

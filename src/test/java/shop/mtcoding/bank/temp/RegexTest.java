bfa4379681260a1cdc435b8e839f406140dabb7d
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-27 17:33:18 +0900

    회원가입 컨트롤러 정규표현식 2

--- src/test/java/shop/mtcoding/bank/temp/RegexTest.java
+++ src/test/java/shop/mtcoding/bank/temp/RegexTest.java
@@ -14,51 +14,36 @@ public class RegexTest {
 
      @Test
      public void 한글은안된다test() throws Exception {
-          // given
-
-          // when
-
-          // then
-
+          String value = "abc";
+          boolean result = Pattern.matches("^[^ㄱ-ㅎ가-힣]*$", value);
+          System.out.println("테스트 : " + result);
      }
 
      @Test
      public void 영어만된다_test() throws Exception {
-          // given
-
-          // when
-
-          // then
-
+          String value = "ssar";
+          boolean result = Pattern.matches("^[a-zA-Z]*$", value);
+          System.out.println("테스트 : " + result);
      }
 
      @Test
      public void 영어는안된다_test() throws Exception {
-          // given
-
-          // when
-
-          // then
-
+          String value = "가22";
+          boolean result = Pattern.matches("^[^a-zA-Z]*$", value);
+          System.out.println("테스트 : " + result);
      }
 
      @Test
      public void 영어와숫자만된다_test() throws Exception {
-          // given
-
-          // when
-
-          // then
-
+          String value = "ab12";
+          boolean result = Pattern.matches("^[a-zA-Z0-9]+$", value);
+          System.out.println("테스트 : " + result);
      }
 
      @Test
      public void 영어만되고_길이는최소2최대4이다_test() throws Exception {
-          // given
-
-          // when
-
-          // then
-
+          String value = "ssar";
+          boolean result = Pattern.matches("^[a-zA-Z]{2,4}$", value);
+          System.out.println("테스트 : " + result);
      }
 }

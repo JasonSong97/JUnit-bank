46ecbf1b4bad0418ff710e74465bdbc6010adab4
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-28 11:58:47 +0900

    본인계좌목록보기 서비스 만들기

--- src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/mtcoding/bank/domain/account/AccountRepository.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.bank.domain.account;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,9 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
      // jpa query method
      // select * from account where number = :number
      Optional<Account> findByNumber(Long number);
+
+     // jpa query method
+     // select * from account where user_id = :id
+     List<Account> findByUser_id(Long id);
 
 }

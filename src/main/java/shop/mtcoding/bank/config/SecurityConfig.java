937d5d5520e5296e461441244030354c6553d777
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-04-29 19:42:06 +0900

    CORS 테스트

--- src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
+++ src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
           configuration.addAllowedMethod("*"); // GET POST PUT DELETE 모든 요청 허용
           configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용(프론트 엔드 IP만 허용 react)
           configuration.setAllowCredentials(true); // client 쪽에서 쿠키 요청 허용
-
+          configuration.addExposedHeader("Authorization"); // 디폴트(예전), 지금은 아님
           UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
           source.registerCorsConfiguration("/**", configuration); // 모든 요청에 해당 과정을 넣겠다.
           return source;

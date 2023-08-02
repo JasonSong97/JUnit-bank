2253a0f3d4645a34759c56e892ff55576e285ecc
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-02 15:11:23 +0900

    reset all===================3rd try==================

--- src/main/java/shop/mtcoding/bank/handler/ex/CustomApiException.java
+++
@@ -1,7 +0,0 @@
-package shop.mtcoding.bank.handler.ex;
-
-public class CustomApiException extends RuntimeException {
-     public CustomApiException(String message) {
-          super(message);
-     }
-}
--- src/main/java/shop/mtcoding/bank/handler/ex/CustomForbiddenException.java
+++
@@ -1,8 +0,0 @@
-package shop.mtcoding.bank.handler.ex;
-
-// 나중에 사용할 예정
-public class CustomForbiddenException extends RuntimeException {
-     public CustomForbiddenException(String message) {
-          super(message);
-     }
-}

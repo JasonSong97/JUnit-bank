293a4c3c05496df277eb918e7827977825892369
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-03 21:07:55 +0900

    setting : SecurityConfig 세팅

--- src/main/java/shop/mtcoding/bank/BankApplication.java
+++ src/main/java/shop/mtcoding/bank/BankApplication.java
@@ -2,7 +2,6 @@ package shop.mtcoding.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // createAt, updateAt
@@ -10,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class BankApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(BankApplication.class, args);
+		SpringApplication.run(BankApplication.class, args);
 
 	}
 }
---
+++ src/main/java/shop/mtcoding/bank/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package shop.mtcoding.bank.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import shop.mtcoding.bank.domain.user.UserEnum;
+
+@Configuration
+public class SecurityConfig {
+
+     private final Logger log = LoggerFactory.getLogger(getClass()); // @slf4j -> JUnit 테스트 문제 발생
+
+     @Bean // IOC에 BCryptPasswordEncoder 객체 등록, @Configuration 붙어있는 곳만
+     public BCryptPasswordEncoder bCryptPasswordEncoder() {
+          log.debug("디버그 : BCryptPasswordEncoder 빈 등록됨");
+          return new BCryptPasswordEncoder();
+     }
+
+     // JWT 등록이 필요함!!
+
+     // JWT 서버 -> 세션 사용 X(무상태 서버)
+     @Bean
+     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+          http.headers().frameOptions().disable(); // iframe 허용 X
+          http.csrf().disable();
+          http.cors().configurationSource(configurationSource());
+
+          // jSessionId 서버쪽에서 관리 안하겠다는 것
+          http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+          // react, app 요청 예정
+          http.formLogin().disable();
+          // 브라우저가 팝업창을 이용해서 사용자 인증을 진행
+          http.httpBasic().disable();
+          http.authorizeRequests()
+                    .antMatchers("/api/s/**").authenticated()
+                    .antMatchers("/api/admin/**").hasRole("" + UserEnum.ADMIN) // ROLE_
+                    .anyRequest().permitAll();
+
+          return http.build();
+     }
+
+     public CorsConfigurationSource configurationSource() {
+          CorsConfiguration configuration = new CorsConfiguration();
+          configuration.addAllowedHeader("*"); // 모든 헤더 받기
+          configuration.addAllowedMethod("*"); // GET, POST, DELETE, PUT 전부 허용
+          configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (FE IP만 허용)
+          configuration.setAllowCredentials(true); // 클라이언트쪽에서 쿠키 요청 허용(클라이언트쪽에서 보내는게 가능)
+
+          UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+          source.registerCorsConfiguration("/**", configuration); // 모든 주소요청에 위에 설정을 넣기
+          return source;
+     }
+}

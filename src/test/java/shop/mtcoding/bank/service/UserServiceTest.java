0ae59bda7bdfcb660b9fe2bcdccc1298b503cfff
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-04 11:56:10 +0900

    test : 회원가입 서비스 테스트

---
+++ src/test/java/shop/mtcoding/bank/service/UserServiceTest.java
@@ -0,0 +1,66 @@
+package shop.mtcoding.bank.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserEnum;
+import shop.mtcoding.bank.domain.user.UserRepository;
+import shop.mtcoding.bank.service.UserService.JoinRequestDto;
+import shop.mtcoding.bank.service.UserService.JoinResponseDto;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+     @InjectMocks // 가짜환경에 넣는 것, @Autowired랑 다름
+     private UserService userService;
+     @Mock
+     private UserRepository userRepository;
+     @Spy
+     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+     @Test
+     public void 회원가입_test() throws Exception {
+          // given
+          JoinRequestDto joinRequestDto = new JoinRequestDto();
+          joinRequestDto.setUsername("ssar");
+          joinRequestDto.setPassword("1234");
+          joinRequestDto.setEmail("ssar@nate.com");
+          joinRequestDto.setFullname("쌀");
+
+          // stub 1
+          when(userRepository.findByUsername(any())).thenReturn(Optional.empty());
+
+          // stub 2
+          User ssar = User.builder()
+                    .id(1L)
+                    .username("ssar")
+                    .password("1234")
+                    .email("ssar@nate.com")
+                    .fullname("쌀")
+                    .role(UserEnum.CUSTOMER)
+                    .createAt(LocalDateTime.now())
+                    .updateAt(LocalDateTime.now())
+                    .build();
+          when(userRepository.save(any())).thenReturn(ssar);
+
+          // when
+          JoinResponseDto joinResponseDto = userService.회원가입(joinRequestDto);
+
+          // then
+          assertThat(joinResponseDto.getId()).isEqualTo(1L);
+          assertThat(joinResponseDto.getUsername()).isEqualTo("ssar");
+     }
+}

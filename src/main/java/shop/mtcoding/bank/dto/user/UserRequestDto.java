d827053809a9d1d8ee8437e741dbb34cdae99a13
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-04 13:55:38 +0900

    refactor : joinDto 정규표현식 추가

--- src/main/java/shop/mtcoding/bank/dto/user/UserRequestDto.java
+++ src/main/java/shop/mtcoding/bank/dto/user/UserRequestDto.java
@@ -1,6 +1,8 @@
 package shop.mtcoding.bank.dto.user;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -15,12 +17,16 @@ public class UserRequestDto {
      @Setter
      public static class JoinRequestDto {
 
+          @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요. ")
           @NotEmpty // null, empty 불가능
           private String username;
+          @Size(min = 4, max = 20)
           @NotEmpty
           private String password;
+          @Pattern(regexp = "^[a-zA-Z0-9]{2,10}@[a-zA-Z0-9]{2,6}\\.[a-zA-Z]{2,3}$", message = "이메일 형식으로 작성해주세요. ")
           @NotEmpty
           private String email;
+          @Pattern(regexp = "^[a-zA-Z가-힣]{1,20}$", message = "한글/영문 1~20자 이내로 작성해주세요. ")
           @NotEmpty
           private String fullname;
 

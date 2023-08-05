ea48fa52856923376f8f7dd5f75c2942cd0b8e9a
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-05 15:33:24 +0900

    test : authorization 테스트

--- src/main/java/shop/mtcoding/bank/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/shop/mtcoding/bank/config/jwt/JwtAuthorizationFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,6 +20,8 @@ import shop.mtcoding.bank.config.auth.LoginUser;
 // 모든 주소에서 동작(검증)
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
+     private final Logger log = LoggerFactory.getLogger(getClass());
+
      public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
           super(authenticationManager);
      }
@@ -26,15 +30,17 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
      protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
                throws IOException, ServletException {
           if (isHeaderVerify(request, response)) {
+               log.debug("디버그 : 토큰이 존재함");
                // 토큰 존재
                String token = request.getHeader(JwtValueObject.HEADER).replace(JwtValueObject.TOKEN_PREFIX, "");
                LoginUser loginUser = JwtProcess.verify(token); // id, role만 존재
+               log.debug("디버그 : 토큰검증이 완료됨");
 
                // 임시 세션, 인증처럼 UserDetailsService 호출 X, UserDetails or username
                Authentication authentication = new UsernamePasswordAuthenticationToken(loginUser, null,
                          loginUser.getAuthorities());
                SecurityContextHolder.getContext().setAuthentication(authentication);
-
+               log.debug("디버그 : 임시 세션이 생성됨");
           }
           chain.doFilter(request, response);
      }

9d06cac2db6e8be110405aaa2aa79a672a9cad81
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-08-08 12:57:13 +0900

    fix : JWT 테스트 만료시간 해결

--- src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
@@ -11,6 +11,13 @@ import shop.mtcoding.bank.domain.user.UserEnum;
 
 public class JwtProcessTest {
 
+     private String createToken() {
+          User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
+          LoginUser loginUser = new LoginUser(user);
+          String jwtToken = JwtProcess.create(loginUser);
+          return jwtToken;
+     }
+
      @Test
      public void create_test() throws Exception {
           // given
@@ -18,7 +25,7 @@ public class JwtProcessTest {
           LoginUser loginUser = new LoginUser(user);
 
           // when
-          String jwtToken = JwtProcess.create(loginUser);
+          String jwtToken = createToken();
           System.out.println("테스트 : " + jwtToken);
 
           // then
@@ -28,7 +35,8 @@ public class JwtProcessTest {
      @Test
      public void verify_test() throws Exception {
           // given
-          String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkNVU1RPTUVSIiwiaWQiOjEsImV4cCI6MTY5MTgwNjIwOH0.z0NyhELnJlWuUmNb4H9oO-uy3ZIHi69VSi3c8QZFH-Ni_grKIPCkPE1paTzYm8UKWeSkelmwJO6IH_TnBzNHDA";
+          String token = createToken(); // Bearer 제거해서 처리
+          String jwtToken = token.replace(JwtValueObject.TOKEN_PREFIX, "");
 
           // when
           LoginUser loginUser = JwtProcess.verify(jwtToken);
